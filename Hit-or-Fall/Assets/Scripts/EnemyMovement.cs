f771486befbc56bb7097ebace9028ffb84321f16
streetzy <57232857+streetzy@users.noreply.github.com>
2021-11-30 11:39:16 +0100

    V0.5.2
    Configuring enemy movement
    Mainly attempting to change velocity

--- Hit-or-Fall/Assets/Scripts/EnemyMovement.cs
+++ Hit-or-Fall/Assets/Scripts/EnemyMovement.cs
@@ -4,22 +4,51 @@ using UnityEngine;
 
 public class TargetMovement : MonoBehaviour
 {
+    private float targetZAxis = 0;
     private Rigidbody targetRigidbody;
+    private GameObject target;
+    private string targetTag;
 
     private void Start()
     {
-        targetRigidbody = gameObject.GetComponent<Rigidbody>();
+        targetTag = gameObject.tag;
+
+        switch (targetTag)
+        {
+            case "Large":
+                target = GameObject.FindGameObjectWithTag("Large");
+                break;
+            case "Mid":
+                target = GameObject.FindGameObjectWithTag("Mid");
+                break;
+            case "Small":
+                target = GameObject.FindGameObjectWithTag("Small");
+                break;
+        }
+        targetRigidbody = target.GetComponent<Rigidbody>();
+        targetRigidbody.velocity = new Vector3(0, 0, -3);
+        StartCoroutine(FasterMovement());
     }
 
     private void Update()
     {
-        targetRigidbody.velocity = new Vector3(0, 0, -5);
         if (gameObject.transform.position.z <= -15f)
         {
             Destroy(gameObject);
         }
     }
 
-    
+    IEnumerator FasterMovement()
+    {
+        while (targetZAxis < 10) 
+        {
+            yield return new WaitForSeconds(2);
+            targetZAxis = targetRigidbody.velocity.z;
+            targetRigidbody.velocity = new Vector3(0, 0, targetZAxis - 3);
+            Debug.Log("Works!");
+        }
+    }
+
+    //Main idea for this script is to create Target Movement by itself, and then slowly change the vel. over time.
 
 }
